91ccba72e2f7374057b8e5177fb531ce645af33d
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-23 21:52:48 +0200

    Update Villager.java

--- src/main/java/efs.task.oop/Villager.java
+++ src/main/java/efs.task.oop/Villager.java
@@ -1,12 +1,38 @@
 package efs.task.oop;
 
-public class Villager {
+public class Villager implements Fighter{
+    protected int health = 100;
     private String name;
     private int age;
 
     public Villager(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    @Override
+    public void attack(Fighter victim) {
+        victim.takeHit((int) (100 - age * 0.5) / 10);
+    }
+
+    @Override
+    public void takeHit(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            health=0;
+        }
     }
 
     public void sayHello() {
