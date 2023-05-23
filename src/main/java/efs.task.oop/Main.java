23728e9048383ee8a1569a2969c33f7a56208b0e
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-23 17:30:24 +0200

    Update Main.java

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -1,21 +1,19 @@
 package efs.task.oop;
 
-public class Villager {
-    private String name;
-    private int age;
-
-    public Villager(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void sayHello() {
-        System.out.println("Greetings traveler... I'm " + name + " and I'm " + age + " years old.");
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
+        Villager kashya = new Villager("Kashya", 30);
+        Villager akara = new Villager("Akara", 40);
+        Villager gheed = new Villager("Gheed", 50);
+        Villager deckardCain = new Villager("Deckard Cain", 85);
+        Villager warriv = new Villager("Warriv", 35);
+        Villager flawia = new Villager("Flawia", 25);
 
+        kashya.sayHello();
+        akara.sayHello();
+        gheed.sayHello();
+        deckardCain.sayHello();
+        warriv.sayHello();
+        flawia.sayHello();
     }
 }
