ca385e382e5bc250f40edf9c771dd40c41825e9a
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-23 17:29:14 +0200

    Create Villager.java

---
+++ src/main/java/efs.task.oop/Villager.java
@@ -0,0 +1,15 @@
+package efs.task.oop;
+
+public class Villager {
+    private String name;
+    private int age;
+
+    public Villager(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void sayHello() {
+        System.out.println("Greetings traveler... I'm " + name + " and I'm " + age + " years old.");
+    }
+}
