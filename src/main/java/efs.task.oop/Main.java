52daf305eca1bf7f9b65bac4948cd2b7eb762199
zywczak <123404938+zywczak@users.noreply.github.com>
2023-05-23 21:51:03 +0200

    Update Main.java

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -1,19 +1,68 @@
 package efs.task.oop;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Villager kashya = new Villager("Kashya", 30);
-        Villager akara = new Villager("Akara", 40);
-        Villager gheed = new Villager("Gheed", 50);
-        Villager deckardCain = new Villager("Deckard Cain", 85);
-        Villager warriv = new Villager("Warriv", 35);
-        Villager flawia = new Villager("Flawia", 25);
+        Villager[] villagers = {
+                new Villager("Kashya", 30),
+                new ExtraordinaryVillager("Akara", 40, ExtraordinaryVillager.Skill.SHELTER),
+                new Villager("Gheed", 50),
+                new ExtraordinaryVillager("Deckard Cain", 85, ExtraordinaryVillager.Skill.IDENTIFY),
+                new Villager("Warriv", 35),
+                new Villager("Flawia", 25)
+        };
 
-        kashya.sayHello();
-        akara.sayHello();
-        gheed.sayHello();
-        deckardCain.sayHello();
-        warriv.sayHello();
-        flawia.sayHello();
+        for (Villager villager : villagers) {
+            villager.sayHello();
+        }
+
+        Object objectDeckardCain = villagers[3];
+        Object objectAkara = villagers[1];
+        //objectDeckardCain.sayHello();//nie mozna wywolac metody z klasy ExtraordinaryVillager
+        //nalezy wykonc rzutowanie ((ExtraordinaryVillager) objectAkara).takeHit(damage);
+
+        Monsters monsters = new Monsters();
+        int i = 0;
+        int n = villagers.length;
+
+        while (monsters.monstersHealth > 0 && villagers[0].getHealth() > 0) {
+            Random random = new Random();
+            i = random.nextInt(n);
+
+            if(monsters.Andariel.getHealth() > 0) {
+                villagers[i].attack(monsters.Andariel);
+            }else{
+                villagers[i].attack(monsters.Blacksmith);
+            }
+
+            if(monsters.Andariel.getHealth()>0) {
+                monsters.Andariel.attack(villagers[i]);
+            }else {
+                monsters.Blacksmith.attack(villagers[i]);
+            }
+
+            if(villagers[i].getHealth() == 0){
+                Villager temp = villagers[n-1];
+                villagers[n-1] = villagers[i];
+                villagers[i] = temp;
+                n--;
+            }
+
+            System.out.println("Potwory posiadają jeszcze " + monsters.monstersHealth + " punkty życia ");
+            System.out.println("Aktualnie walczący osadnik to " + villagers[i].getName());
+        }
+
+        if(villagers[0].getHealth() > 0){
+            System.out.println("Obozowisko ocalone!");
+        }else{
+            System.out.println("Obozowisko nie ocalone!");
+        }
+
+        for (Villager villager : villagers) {
+            if (villager instanceof ExtraordinaryVillager) {
+                ExtraordinaryVillager extraordinaryVillager = (ExtraordinaryVillager) villager;
+                extraordinaryVillager.sayHello();
+            }
+        }
     }
 }
